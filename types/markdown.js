c575d3b0b3d1105365704155608a3460b784cc07
David Konsumer <konsumer@jetboystudio.com>
2020-10-09 15:43:58 -0700

    more ideas

---
+++ types/markdown.js
@@ -0,0 +1,11 @@
+// turn a string into a data-object
+export const contentToData = content => {}
+
+// turn a data-object into a string
+export const dataToContent = data => {}
+
+// some field-formatters
+export const TypeFrontmatterText = ({ field, value, onChange }) => <div>TypeFrontmatterText</div>
+export const TypeFrontmatterDate = ({ field, value, onChange }) => <div>TypeFrontmatterDate</div>
+export const TypeFrontmatterStringArray = ({ field, value, onChange }) => <div>TypeFrontmatterStringArray</div>
+export const TypeMarkdownBody = ({ field, value, onChange }) => <div>TypeMarkdownBody</div>
